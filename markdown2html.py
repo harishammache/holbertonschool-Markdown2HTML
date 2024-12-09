097a9e87b88724099883c613890183f6050a6e27
harishammache <hammache_haris@hotmail.fr>
2024-12-09 13:27:53 +0100

    script markdown2html.py that takes an argument 2 strings

---
+++ markdown2html.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from sys import argv, exit, stderr
+import os
+
+"""script that tanked an argument 2 strings"""
+
+
+def main():
+    """tanked an argument 2 strings"""
+    if len(argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
+        exit(1)
+        
+    markdown_file = argv[1]
+    outpout_file_name = argv[2]
+
+    if os.path.exists(markdown_file):
+        print(f"Missing {markdown_file}", file=stderr)
+        exit(1)
+
+    else:
+        exit(0)
