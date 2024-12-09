449c5f30c968cdf312b4ff397701b1039e10261f
harishammache <hammache_haris@hotmail.fr>
2024-12-09 14:03:15 +0100

    change the syntax

--- markdown2html.py
+++ markdown2html.py
@@ -8,7 +8,6 @@ import os
 
 def main():
     """tanked an argument 2 strings"""
-
     if len(argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
